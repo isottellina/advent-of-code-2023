58c2109058fd60d610cc1d83e6c39e2d59dddadb
Louise <louise@zanier.org>
2023-12-09 10:21:12 +0100

    day 9 first part

---
+++ day9-1.el
@@ -0,0 +1,23 @@
+;; -*- lexical-binding: t; -*-
+
+(require 'dash)
+
+(defun day9/get-next-value (seq)
+  (cl-labels ((list-of-differences (seq)
+                (cl-loop for idx from 0
+                         for current-cdr = (nthcdr idx seq)
+                         while (cdr current-cdr)
+                         collect (- (cadr current-cdr) (car current-cdr))))
+              (next-value (seq
+                           &aux (distinct-seq (-distinct seq)))
+                (if (= (length distinct-seq) 1)
+                    (car distinct-seq)
+                  (+ (-last-item seq) (next-value (list-of-differences seq))))))
+    (next-value seq)))
+
+(with-current-buffer "input.txt"
+  (goto-char (point-min))
+  (cl-loop until (and (eolp) (bolp))
+           sum (day9/get-next-value (cl-loop while (re-search-forward "-?[[:digit:]]+" (line-end-position) t)
+                                             collect (string-to-number (match-string 0))))
+           do (forward-line)))
